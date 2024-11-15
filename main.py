a48fe1288d7ec900d4fcf87a71e57559e1317927
yarmukhametov <@gmail.com>
2024-11-15 13:39:18 +0500

    prompt user for last name as well.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Python pr: "Hello, World!"
 def main():
-    print("Hello, World!")
+    name = input("Enter your name: ")
+    print(f"length = {len(name)}")  # строка для отладки
+    name = name.strip()  # удаляет символ новой строки (и пробелы) в конце
 
 if __name__ == "__main__":
     main()
